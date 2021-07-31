35dbb4af1ef216044009d719a50929c5d83aaa07
loveZero <1120877019@qq.com>
2021-07-31 13:07:06 +0800

    computed计算属性

---
+++ src/08-computed计算属性.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div>字符串---{{ str }}</div>
+    <div>反转后---{{ newStr }}</div>
+  </div>
+</template>
+
+<script>
+import { computed, ref } from 'vue'
+export default {
+  name: 'App',
+  setup () {
+    const str = ref('hello')
+    // 计算属性：根据一定的逻辑得到一个新的数据，且是响应式的
+    const newStr = computed(() => {
+      // 返回新的数据
+      return str.value
+        .split('')
+        .reverse()
+        .join('')
+    })
+    return {
+      str,
+      newStr
+    }
+  }
+}
+</script>

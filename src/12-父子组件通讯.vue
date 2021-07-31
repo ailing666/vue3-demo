961d15a7c1f72b507457663fb3c762f61aad1213
loveZero <1120877019@qq.com>
2021-07-31 16:32:10 +0800

    父子组件通讯

---
+++ src/12-父子组件通讯.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>父组件</h1>
+    {{ num }}
+    <hr />
+    <!-- 组件传参 -->
+    <Son :num="num" @changeNum="changeNum"></Son>
+  </div>
+</template>
+
+<script>
+import Son from './Son.vue'
+import { ref } from 'vue'
+export default {
+  name: 'App',
+  components: { Son },
+  setup () {
+    const num = ref(10)
+    const changeNum = val => {
+      num.value += val
+    }
+    return { num, changeNum }
+  }
+}
+</script>
---
+++ src/Son.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="son">
+    <h1>子组件</h1>
+    {{ num }}
+    <button @click="changeNum">changeNum</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Son',
+  // 接收父组件传的参数
+  props: {
+    num: {
+      type: Number,
+      default: 0
+    }
+  },
+  // 子组件触发父组件的方法
+  setup (props, { emit }) {
+    const changeNum = () => {
+      emit('changeNum', props.num)
+    }
+    return { changeNum }
+  }
+}
+</script>

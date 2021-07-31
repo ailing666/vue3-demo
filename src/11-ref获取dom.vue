8f53641a54ce0dd63a13f043b67765f6f9662da3
loveZero <1120877019@qq.com>
2021-07-31 15:22:28 +0800

    ref获取dom

---
+++ src/11-ref获取dom.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!-- 单个dom -->
+    <div ref="dom">box</div>
+    <!-- 循环dom -->
+    <ul>
+      <li v-for="i in 5" :key="i" :ref="getDom">第{{ i }}个</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue'
+export default {
+  name: 'App',
+  setup () {
+    // 获取单个dom
+    const dom = ref(null)
+    onMounted(() => {
+      console.log(dom.value)
+    })
+
+    // 获取多个dom
+    const domList = []
+    const getDom = el => {
+      domList.push(el)
+    }
+    onMounted(() => {
+      console.log(domList)
+    })
+
+    return {
+      dom,
+      getDom
+    }
+  }
+}
+</script>
